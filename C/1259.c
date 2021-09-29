579156b5ceb216ecb28cf8a6ec559b8541fa1a17
Mat780 <mateusfelipe10@live.com>
2021-09-29 08:54:47 -0400

    Exer: 1259.c

--- C/1259.c
+++ C/1259.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
-#define MAX 100
+#define MAX 1000
 
-void intercala(int p, int q, int r, int v[MAX])
-{
-    int i, j, k, w[MAX];
-    i = p; j = q; k = 0;
-    while (i < q && j < r) {
-        if (v[i] <= v[j]) {
-            w[k] = v[i]; i++; }
-        else {
-            w[k] = v[j]; j++; }
-        k++;
-    }
-    while (i < q) {
-        w[k] = v[i]; i++; k++; }
-    while (j < r) {
-        w[k] = v[j]; j++; k++; }
-    for (i = p; i < r; i++)
-        v[i] = w[i-p];
+void troca(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void mergesort(int p, int r, int v[MAX])
+
+int separa(int p, int r, int v[MAX]){
+    int x, i, j;
+    x = v[p];
+    i = p - 1;
+    j = r + 1;
+    while (i < j) {
+        do {
+            j--;
+        } while (v[j] > x);
+        do {
+            i++;
+        } while (v[i] < x);
+        if (i < j)
+            troca(&v[i], &v[j]);
+    }
+    return j;
+}
+
+void quicksort(int p, int r, int v[MAX])
 {
     int q;
-    if (p < r - 1) {
-        q = (p + r) / 2;
-        mergesort(p, q, v);
-        mergesort(q, r, v);
-        intercala(p, q, r, v);
+    if (p < r) {
+        q = separa(p, r, v);
+        quicksort(p, q, v);
+        quicksort(q+1, r, v);
     }
 }
+
 
 int main(void){
     int vez, num, par = 0, impar = 0;
-    int vP[MAX], vIP[MAX];
     scanf("%d", &vez);
+
+    int vP[vez], vIP[vez];
 
     for (int c = 0; c < vez; c++){
         scanf("%d", &num);
@@ -48,8 +55,8 @@ int main(void){
         }
     }
 
-    mergesort();
-    mergesort(vIP, impar);
+    quicksort(0, par-1, vP);
+    quicksort(0, impar-1, vIP);
 
     for(int c = 0; c < par; c++){
         printf("%d\n", vP[c]);
